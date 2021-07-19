0c82e7dd74f9739cf83e6b8b7622cce90132cce3
Boeun <boeun0904@naver.com>
2021-07-19 22:25:35 +0900

    Add - 하단 메뉴바 생성

---
+++ app/src/main/java/com/example/eat_da/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.eat_da;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private frag_chatting frag_chatting;
+    private frag_eatfolio frag_eatfolio;
+    private frag_matching frag_matching;
+    private frag_restaurant frag_restaurant;
+    private frag_settings frag_settings;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        frag_chatting = new frag_chatting();
+        frag_eatfolio = new frag_eatfolio();
+        frag_matching = new frag_matching();
+        frag_restaurant = new frag_restaurant();
+        frag_settings = new frag_settings();
+        setFrag(0);         //첫 프래그먼트 화면 지정
+
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.action_eatfolio:
+                        setFrag(0);
+                        break;
+                    case R.id.action_matching:
+                        setFrag(1);
+                        break;
+                    case R.id.action_chatting:
+                        setFrag(2);
+                        break;
+                    case R.id.action_restaurant:
+                        setFrag(3);
+                        break;
+                    case R.id.action_settings:
+                        setFrag(4);
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    //프래그먼트 교체가 일어나는 실행문
+    private void setFrag(int n){
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        switch(n){
+            case 0:
+                ft.replace(R.id.main_frame, frag_eatfolio);
+                ft.commit();
+                break;
+            case 1:
+                ft.replace(R.id.main_frame, frag_matching);
+                ft.commit();
+                break;
+            case 2:
+                ft.replace(R.id.main_frame, frag_chatting);
+                ft.commit();
+                break;
+            case 3:
+                ft.replace(R.id.main_frame, frag_restaurant);
+                ft.commit();
+                break;
+            case 4:
+                ft.replace(R.id.main_frame, frag_settings);
+                ft.commit();
+                break;
+        }
+    }
+}
