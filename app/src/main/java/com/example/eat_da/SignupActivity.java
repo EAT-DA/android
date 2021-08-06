a9462f4a8b724248af4e52e770116612932a50c2
Boeun <boeun0904@naver.com>
2021-08-07 01:45:36 +0900

    Mod - 로그인/회원가입 화면 수정2

--- app/src/main/java/com/example/eat_da/SignupActivity.java
+++ app/src/main/java/com/example/eat_da/SignupActivity.java
@@ -6,18 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SignupActivity extends AppCompatActivity {
 
-    private Button bt_back_register;
+    private ImageButton bt_back_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        bt_back_register = findViewById(R.id.bt_back_register);
-        bt_back_register.setOnClickListener(new View.OnClickListener() {
+        bt_back_signup = findViewById(R.id.bt_back_signup);
+        bt_back_signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
